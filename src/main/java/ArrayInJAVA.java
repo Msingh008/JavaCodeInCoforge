8386a03b389ad5d2433fa59358a89ca19f2ecdb9
Manish Kumar - H -  ShantiOne <Manish.47.Kumar@coforge.com>
2025-06-13 13:04:25 +0530

    added file ArrayInJAVA

---
+++ src/main/java/ArrayInJAVA.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayInJAVA {
+    public static void main(String[] args) {
+        Integer [] numbers=new Integer[] {1,6,44,66,33,22};
+        String [] name=new  String[]{"Sumit","Amit","Anand"};
+
+        System.out.println(numbers);
+       // List<Integer> listCopyArr = Arrays.asList(copyArr);
+        System.out.println(Arrays.asList(numbers));
+        //Putting some line of ocde to stash.
+        //Let see how it works.............
+       }
+}
